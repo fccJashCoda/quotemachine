91215fc7ef46733e67e9da306dad0b63f0ea6d5f
fccJashCoda <personalrevolution2014@gmail.com>
2020-11-09 22:57:29 +0100

    add: caching fetch data so we don't spam the api every time we get a new quote

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -5,15 +5,22 @@ function QuoteMachine() {
   const [theme, setTheme] = useState('theme1');
   const [randomQuote, setRandomQuote] = useState('');
   const [trigger, setTrigger] = useState(false);
+  const [cache, setCache] = useState([]);
 
   useEffect(() => {
     const getQuote = async () => {
-      const response = await axios.get(
-        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-      );
-      const newQuote =
-        response.data.quotes[getRandomNumber(response.data.quotes.length)];
-      setRandomQuote(newQuote);
+      if (!cache.length) {
+        const response = await axios.get(
+          'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+        );
+        const newQuote =
+          response.data.quotes[getRandomNumber(response.data.quotes.length)];
+        setRandomQuote(newQuote);
+        setCache(response.data.quotes);
+      } else {
+        const newQuote = cache[getRandomNumber(cache.length)];
+        setRandomQuote(newQuote);
+      }
     };
     getQuote();
     setTrigger(false);
@@ -42,16 +49,18 @@ function QuoteMachine() {
           <p id="text">{quote}</p>
           <p id="author">- {author}</p>
         </blockquote>
-        <a href="twitter.com/intent/tweet" id="tweet-quote">
-          <i className="icon fab fa-twitter-square"></i>
-        </a>
-        <button
-          onClick={() => getNewQuote()}
-          id="new-quote"
-          className="btn btn-primary"
-        >
-          New Quote
-        </button>
+        <div className="buttons">
+          <a href="twitter.com/intent/tweet" id="tweet-quote">
+            <i className="icon fab fa-twitter-square"></i>
+          </a>
+          <button
+            onClick={() => getNewQuote()}
+            id="new-quote"
+            className="btn btn-primary"
+          >
+            New Quote
+          </button>
+        </div>
       </div>
     </div>
   );
